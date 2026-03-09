bd4afb2df8835f7e9533477f1ae173dc815406f0
jhunadev <jhunalbisdeyanquintero@gmail.com>
2026-03-09 20:36:21 +0000

    tarjeta terminada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+  let randomValueIndex = Math.floor(Math.random() * values.length);
+
+  let finalSuit = suits[randomSuitIndex];
+  let finalValue = values[randomValueIndex];
+
+  document.getElementById("top-suit").innerHTML = finalSuit;
+  document.getElementById("number").innerHTML = finalValue;
+  document.getElementById("bottom-suit").innerHTML = finalSuit;
+
+  if (finalSuit === "♥" || finalSuit === "♦") {
+    document.getElementById("card-container").style.color = "red";
 };
+}
